9c068d3832316b3ef0395d8999e9d23f19ad4adf
Bon Chau <tccha17@student.monash.edu>
2019-10-24 14:50:37 +1100

    try setting up tank player as a player ctrler.

--- MyProjectA/Source/MyProject/MyProjectGameMode.cpp
+++ MyProjectA/Source/MyProject/MyProjectGameMode.cpp
@@ -7,7 +7,8 @@
 AMyProjectGameMode::AMyProjectGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/mytankPlayer"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
